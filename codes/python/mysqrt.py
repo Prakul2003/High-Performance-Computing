a643dc3320ed119a3cb4861a7f8712b3d95162d7
Prakul Khurana <Prakul2003@users.noreply.github.com>
2024-03-19 19:07:26 +0530

    debug flag added and return value

--- codes/python/mysqrt.py
+++ codes/python/mysqrt.py
@@ -3,17 +3,21 @@
 Module to calculate sqrt using the Newton's Method
 
 """
-def sqrt2(x):
+def sqrt2(x,debug=False):
  """
  sqrt function implementation
  """
  s=1.0
  tol=2.2e-16
  for k in range(100):
-  s0=s	
-  print(f"At iteration {k} the value of s = {s:20.15f}")
+  s0=s
+  if(debug):	
+   print(f"At iteration {k} the value of s = {s:20.15f}")
   s=0.5*(s+x/s)
   delta_s=abs(s-s0)
   if(delta_s/x < tol):
    break
- print(f"finally the value of sqrt is {s:20.15f}")
+ if(debug):
+  print(f"finally the value of sqrt is {s:20.15f}")
+ return s
+
