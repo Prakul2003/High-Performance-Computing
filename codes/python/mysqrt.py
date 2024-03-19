0b5bc23edfb907e8303fef024309b0af048da788
Prakul Khurana <Prakul2003@users.noreply.github.com>
2024-03-19 17:28:19 +0530

    modified mysqrt

--- codes/python/mysqrt.py
+++ codes/python/mysqrt.py
@@ -1,8 +1,14 @@
-# Newton Method
-x=2.0
+# Newton Method for computing square root
+x=0.0001
 s=1.0
-for k in range(5):	
+tol=2.2e-16
+for k in range(100):
+ s0=s	
  print(f"At iteration {k} the value of s = {s:17.15f}")
  s=0.5*(s+x/s)
+ delta_s=abs(s-s0)
+ if(delta_s/x < tol):
+  break
+
 
 print(f"finally the value of sqrt is {s}")
