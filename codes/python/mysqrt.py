cc3c1c2e9bf5b1f76bc448cb3f74a073dfc4f414
Prakul Khurana <Prakul2003@users.noreply.github.com>
2024-03-19 17:15:13 +0530

    modified mysqrt

--- codes/python/mysqrt.py
+++ codes/python/mysqrt.py
@@ -1,7 +1,8 @@
 # Newton Method
-x=9.0
+x=2.0
 s=1.0
-for k in range(5):
-    s=0.5*(s+x/s)
+for k in range(5):	
+ print(f"At iteration {k} the value of s = {s:17.15f}")
+ s=0.5*(s+x/s)
 
-print(s)
+print(f"finally the value of sqrt is {s}")
